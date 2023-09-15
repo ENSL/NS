7210502cd7e46b23f5a1c14b5e65169da523756d
RGreenlees <RGreenlees@users.noreply.github.com>
2023-09-15 11:35:37 +0100

    Prevent duplicate tooltips appearing on screen
    Tooltips will no longer be added to the screen if that message is already being displayed. This prevents the same message (e.g. the ready room F4 message) from appearing multiple times if the player keeps pressing F4.

--- main/source/mod/AvHTooltip.h
+++ main/source/mod/AvHTooltip.h
@@ -76,6 +76,7 @@ public:
 	void		SetBoldR(int inR);
 	void		SetBoldG(int inG);
 	void		SetBoldB(int inB);
+	string		GetText() { return mText; }
 				
 private:		
 	bool		ChopStringOfMaxScreenWidth(int inMaxScreenWidth, string& ioBaseString, string& outChoppedString);
